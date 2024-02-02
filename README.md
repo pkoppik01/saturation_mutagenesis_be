4804e835f6152f910b3a48096b9f9597844b6990
Pratik Koppikar <93696321+pkoppik01@users.noreply.github.com>
2024-02-02 10:12:07 -0500

    Added additional position checks and cleaned
    checks for possible edit at pos 4 and 6. updated readme with details on how exons were selected.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # saturation_mutagenesis_be
- When given a text file of base pairs, this will return it as all possible guide RNAs for editing the 5th position with ABEs and CBEs.
+ When given a text file of base pairs, this will return it as all possible guide RNAs for editing the 4th, 5th, or 6th position with ABEs and CBEs.
+ These positions were determined empirically to be the highest efficiency edits.
+
+ Exons 2,3,4 from APOE were used to generate saturation mutagenesis gRNAs.
+ When selecting sequences from exons, 20bp excess was included on both sides as a buffer. This ensures that both forward and reverse guides can be correctly assigned for possible edits on the edges of the exons.
+
+ All of exon 2 was used, including non-signal sequence/non-CDS. All of exon 3 was used. Only the coding sequence of exon 4 was used, and the STOP codon was included in the non-buffer area (can be possibly mutated)
--- protospacer_qc.py
+++ protospacer_qc.py
@@ -5,7 +5,7 @@ abe_editable_protospacers = []
 cbe_editable_protospacers = []
 
 # Read the CSV file
-file_path = '/Users/koppikarps/Python Development/saturation_mutagenesis_be/test.saturation_mutagenesis_protospacers.2024-01-29.csv'
+file_path = '/Users/koppikarps/Python Development/saturation_mutagenesis_be/test.saturation_mutagenesis_protospacers.2024-02-02.csv'
 with open(file_path, 'r') as file:
     csv_reader = csv.DictReader(file)
 
@@ -16,6 +16,8 @@ with open(file_path, 'r') as file:
         run_type = row['run_type']
 
         # Check if the protospacer is 20 characters long
+        # Possible efficient edit window determined from  https://doi.org/10.1038/s41587-019-0193-0 Figure 5b
+        # Checks for possible edit at position 4/5/6 and if a like base pair is directly adjacent
         if len(protospacer) == 20:
             fifth_char = protospacer[4]
 
@@ -23,16 +25,40 @@ with open(file_path, 'r') as file:
             if fifth_char == 'C':
                 # Filter out protospacers with the same character immediately to the left or right
                 if protospacer[3] != 'C' and protospacer[5] != 'C':
-                    cbe_editable_protospacers.append({'protospacer': protospacer, 'position': position, 'run_type': run_type})
+                    cbe_editable_protospacers.append({'protospacer': protospacer, 'position': position, 'run_type': run_type, 'edit_pos': 5})
             elif fifth_char == 'A':
                 # Filter out protospacers with the same character immediately to the left or right
                 if protospacer[3] != 'A' and protospacer[5] != 'A':
-                    abe_editable_protospacers.append({'protospacer': protospacer, 'position': position, 'run_type': run_type})
+                    abe_editable_protospacers.append({'protospacer': protospacer, 'position': position, 'run_type': run_type, 'edit_pos': 5})
+
+            fourth_char = protospacer[3]
+
+            # Check the 4th character to determine ABE or CBE editable
+            if fourth_char == 'C':
+                # Filter out protospacers with the same character immediately to the left or right
+                if protospacer[2] != 'C' and protospacer[4] != 'C':
+                    cbe_editable_protospacers.append({'protospacer': protospacer, 'position': position, 'run_type': run_type, 'edit_pos': 4})
+            elif fourth_char == 'A':
+                # Filter out protospacers with the same character immediately to the left or right
+                if protospacer[2] != 'A' and protospacer[4] != 'A':
+                    abe_editable_protospacers.append({'protospacer': protospacer, 'position': position, 'run_type': run_type, 'edit_pos': 4})
+
+            sixth_char = protospacer[5]
+
+            # Check the 5th character to determine ABE or CBE editable
+            if sixth_char == 'C':
+                # Filter out protospacers with the same character immediately to the left or right
+                if protospacer[4] != 'C' and protospacer[6] != 'C':
+                    cbe_editable_protospacers.append({'protospacer': protospacer, 'position': position, 'run_type': run_type, 'edit_pos': 6})
+            elif sixth_char == 'A':
+                # Filter out protospacers with the same character immediately to the left or right
+                if protospacer[4] != 'A' and protospacer[6] != 'A':
+                    abe_editable_protospacers.append({'protospacer': protospacer, 'position': position, 'run_type': run_type, 'edit_pos': 6})
 
 
 # Write the results to two separate CSV files
-abe_file_path = 'abe_editable_protospacers.csv'
-cbe_file_path = 'cbe_editable_protospacers.csv'
+abe_file_path = 'abe_editable_protospacers2.csv'
+cbe_file_path = 'cbe_editable_protospacers2.csv'
 
 # Function to write rows with headers
 def write_csv(file_path, data, headers):
@@ -42,11 +68,10 @@ def write_csv(file_path, data, headers):
         csv_writer.writerows(data)
 
 # Write ABE editable protospacers to CSV
-abe_headers = ['protospacer', 'position', 'run_type']
+abe_headers = ['protospacer', 'position', 'run_type', 'edit_pos']
 write_csv(abe_file_path, abe_editable_protospacers, abe_headers)
 
 # Write CBE editable protospacers to CSV
-cbe_headers = ['protospacer', 'position', 'run_type']
+cbe_headers = ['protospacer', 'position', 'run_type', 'edit_pos']
 write_csv(cbe_file_path, cbe_editable_protospacers, cbe_headers)
 
-print('hi')
